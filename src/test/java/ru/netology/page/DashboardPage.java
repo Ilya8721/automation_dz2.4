b121b12e2b7de9a10e2b534f7d38c9927812f136
Ilya Bobrov <i.bobrov87@gmail.com>
2023-05-17 15:46:43 +0300

    changed

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,56 +1,51 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
+import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
   private SelenideElement heading = $("[data-test-id=dashboard]");
-  private SelenideElement firstCard = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
-  private SelenideElement secondCard = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
+  private ElementsCollection cards = $$("li div[data-test-id]");
   private final String balanceStart = "баланс: ";
   private final String balanceFinish = " р.";
+  private String cssLocatorForTopUpButton = "[data-test-id='action-deposit']";
   private SelenideElement pageRefresh = $("[data-test-id='action-reload']");
-  private SelenideElement topUpFirstBalanceButton = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit']");
-  private SelenideElement topUpSecondBalanceButton = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] [data-test-id='action-deposit']");
+
 
   public DashboardPage() {
     heading.shouldBe(visible);
   }
 
-  public int getFirstCardBalance() {
-    val text = firstCard.text();
-    return extractBalanceFirstCard(text);
-  }
-
-  private int extractBalanceFirstCard(String text) {
-    val start = text.indexOf(balanceStart);
-    val finish = text.indexOf(balanceFinish);
-    val value = text.substring(start + balanceStart.length(), finish);
-    return Integer.parseInt(value);
-  }
-
-  public int getSecondCardBalance() {
-    val text = secondCard.text();
-    return extractBalanceSecondCard(text);
-  }
-
-  private int extractBalanceSecondCard(String text) {
-    val start = text.indexOf(balanceStart);
-    val finish = text.indexOf(balanceFinish);
-    val value = text.substring(start + balanceStart.length(), finish);
-    return Integer.parseInt(value);
-  }
-
-  public MoneyTransfer topUpFirstBalanceButtonClick() {
-    topUpFirstBalanceButton.click();
+  public MoneyTransfer topUpButtonClick(DataHelper.CardNumber card) {
+    findCard(card).$(cssLocatorForTopUpButton).click();
     return new MoneyTransfer();
   }
 
-  public MoneyTransfer topUpSecondBalanceButtonClick() {
-    topUpSecondBalanceButton.click();
-    return new MoneyTransfer();
+  public SelenideElement findCard(DataHelper.CardNumber card) {
+    String cardNumber = card.getCardNumber().substring(15);
+    for (SelenideElement element : cards) {
+      if (element.text().contains(cardNumber)) {
+        return element;
+      }
+    }
+    return null;
+  }
+
+  public int getCardBalance(DataHelper.CardNumber card) {
+    val text = findCard(card).text();
+    return extractBalance(text);
+  }
+
+  private int extractBalance(String text) {
+    int start = text.indexOf(balanceStart);
+    int finish = text.indexOf(balanceFinish);
+    String value = text.substring(start + balanceStart.length(), finish);
+    return Integer.parseInt(value);
   }
 }
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -10,6 +10,8 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
+  DataHelper.CardNumber card1 = DataHelper.getFirstCardNumber();
+  DataHelper.CardNumber card2 = DataHelper.getSecondCardNumber();
 
   @BeforeEach
   void shouldTransferMoneyBetweenOwnCardsV2() {
@@ -25,43 +27,41 @@ class MoneyTransferTest {
   void transferFromFirstCardToSecondCard() {
     int amount = 1000;
 
-    var dashboardPage = new DashboardPage();
-    int firstBalance = dashboardPage.getFirstCardBalance();
-    int secondBalance = dashboardPage.getSecondCardBalance();
-    var moneyTransfer = dashboardPage.topUpFirstBalanceButtonClick();
-    var cardNumber = DataHelper.getSecondCardNumber();
-    moneyTransfer.topUpBalance(amount, cardNumber);
+    DashboardPage dashboardPage = new DashboardPage();
+    int firstBalance = dashboardPage.getCardBalance(card1);
+    int secondBalance = dashboardPage.getCardBalance(card2);
+    var moneyTransfer = dashboardPage.topUpButtonClick(card1);
+    moneyTransfer.topUpBalance(amount, card2);
 
-    assertEquals(firstBalance + amount, dashboardPage.getFirstCardBalance());
-    assertEquals(secondBalance - amount, dashboardPage.getSecondCardBalance());
+    assertEquals(firstBalance + amount, dashboardPage.getCardBalance(card1));
+    assertEquals(secondBalance - amount, dashboardPage.getCardBalance(card2));
   }
 
   @Test
   void transferFromSecondCardToFirstCard() {
-    int amount = 1000;
+    int amount = 2000;
 
     var dashboardPage = new DashboardPage();
-    int firstBalance = dashboardPage.getFirstCardBalance();
-    int secondBalance = dashboardPage.getSecondCardBalance();
-    var moneyTransfer = dashboardPage.topUpSecondBalanceButtonClick();
-    var cardNumber = DataHelper.getFirstCardNumber();
-    moneyTransfer.topUpBalance(amount, cardNumber);
+    int firstBalance = dashboardPage.getCardBalance(card1);
+    int secondBalance = dashboardPage.getCardBalance(card2);
+    var moneyTransfer = dashboardPage.topUpButtonClick(card2);
+    moneyTransfer.topUpBalance(amount, card1);
 
-    assertEquals(firstBalance - amount, dashboardPage.getFirstCardBalance());
-    assertEquals(secondBalance + amount, dashboardPage.getSecondCardBalance());
+    assertEquals(firstBalance - amount, dashboardPage.getCardBalance(card1));
+    assertEquals(secondBalance + amount, dashboardPage.getCardBalance(card2));
   }
 
   @Test
   void shouldReturnToDashboardPage1() {
     var dashboardPage = new DashboardPage();
-    var moneyTransfer = dashboardPage.topUpFirstBalanceButtonClick();
+    var moneyTransfer = dashboardPage.topUpButtonClick(card1);
     moneyTransfer.cancelAndReturn();
   }
 
   @Test
   void shouldReturnToDashboardPage2() {
     var dashboardPage = new DashboardPage();
-    var moneyTransfer = dashboardPage.topUpSecondBalanceButtonClick();
+    var moneyTransfer = dashboardPage.topUpButtonClick(card2);
     moneyTransfer.cancelAndReturn();
   }
 
@@ -70,14 +70,13 @@ class MoneyTransferTest {
     int amount = 0;
 
     var dashboardPage = new DashboardPage();
-    int firstBalance = dashboardPage.getFirstCardBalance();
-    int secondBalance = dashboardPage.getSecondCardBalance();
-    var moneyTransfer = dashboardPage.topUpFirstBalanceButtonClick();
-    var cardNumber = DataHelper.getSecondCardNumber();
-    moneyTransfer.topUpBalance(amount, cardNumber);
+    int firstBalance = dashboardPage.getCardBalance(card1);
+    int secondBalance = dashboardPage.getCardBalance(card2);
+    var moneyTransfer = dashboardPage.topUpButtonClick(card1);
+    moneyTransfer.topUpBalance(amount, card2);
 
-    assertEquals(firstBalance + amount, dashboardPage.getFirstCardBalance());
-    assertEquals(secondBalance - amount, dashboardPage.getSecondCardBalance());
+    assertEquals(firstBalance + amount, dashboardPage.getCardBalance(card1));
+    assertEquals(secondBalance - amount, dashboardPage.getCardBalance(card2));
   }
 
   @Test
@@ -85,27 +84,25 @@ class MoneyTransferTest {
     int amount = 0;
 
     var dashboardPage = new DashboardPage();
-    int firstBalance = dashboardPage.getFirstCardBalance();
-    int secondBalance = dashboardPage.getSecondCardBalance();
-    var moneyTransfer = dashboardPage.topUpSecondBalanceButtonClick();
-    var cardNumber = DataHelper.getFirstCardNumber();
-    moneyTransfer.topUpBalance(amount, cardNumber);
+    int firstBalance = dashboardPage.getCardBalance(card1);
+    int secondBalance = dashboardPage.getCardBalance(card2);
+    var moneyTransfer = dashboardPage.topUpButtonClick(card2);
+    moneyTransfer.topUpBalance(amount, card1);
 
-    assertEquals(firstBalance - amount, dashboardPage.getFirstCardBalance());
-    assertEquals(secondBalance + amount, dashboardPage.getSecondCardBalance());
+    assertEquals(firstBalance - amount, dashboardPage.getCardBalance(card1));
+    assertEquals(secondBalance + amount, dashboardPage.getCardBalance(card2));
   }
 
   @Test
   void shouldNotTransferFromFirstCardToSecondCard() {
     var dashboardPage = new DashboardPage();
-    int firstBalance = dashboardPage.getFirstCardBalance();
-    int secondBalance = dashboardPage.getSecondCardBalance();
-    var moneyTransfer = dashboardPage.topUpFirstBalanceButtonClick();
-    var cardNumber = DataHelper.getSecondCardNumber();
-    int amount = secondBalance * 2;
-    moneyTransfer.topUpBalance(amount, cardNumber);
+    int firstBalance = dashboardPage.getCardBalance(card1);
+    int secondBalance = dashboardPage.getCardBalance(card2);
+    var moneyTransfer = dashboardPage.topUpButtonClick(card1);
+    int amount = 30_000;
+    moneyTransfer.topUpBalance(amount, card2);
 
-    assertEquals(firstBalance, dashboardPage.getFirstCardBalance());
-    assertEquals(secondBalance, dashboardPage.getSecondCardBalance());
+    assertEquals(firstBalance, dashboardPage.getCardBalance(card1));
+    assertEquals(secondBalance, dashboardPage.getCardBalance(card2));
   }
 }
